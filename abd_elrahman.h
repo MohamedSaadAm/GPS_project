4a5ef18ad27b11b4612bba5e66191edcbc8d69b5
youssefahmed74 <85295287+youssefahmed74@users.noreply.github.com>
2021-06-06 13:36:04 +0200

    Turn LED ON function

--- abd_elrahman.h
+++ abd_elrahman.h
@@ -1,15 +1,14 @@
-#include "tm4c123gh6pm.h"
-
-void turn_led_on()
+void initportF()
 {
 
   SYSCTL_RCGCGPIO_R |= 0x20;   
-  while ((SYSCTL_PRGPIO_R & 0x20)==0);
-  GPIO_PORTF_LOCK_R = 0x4C4F434B;   
+  while ((SYSCTL_PRGPIO_R & 0x20)==0);   
 	GPIO_PORTF_AMSEL_R &= ~(0x02);
 	GPIO_PORTF_AFSEL_R &= ~(0x02);
 	GPIO_PORTF_PCTL_R &= ~(0x02);
 	GPIO_PORTF_DIR_R |= 0x02;          
 	GPIO_PORTF_DEN_R |= 0x02;
-        GPIO_PORTF_DATA_R |= (0x02);
+}
+void turn_led_on(){
+	  GPIO_PORTF_DATA_R |= (0x02);
 }
