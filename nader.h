555b474a15c5a879d89350ff4f43ac1e42658eac
MohamedNader9999999 <85295227+MohamedNader9999999@users.noreply.github.com>
2021-06-13 19:56:28 +0200

    Update nader.h

--- nader.h
+++ nader.h
@@ -1,44 +1,71 @@
-extern char line[100];          // GPGGA line
-extern int i;                   // iterator on the line
-extern char* line_cutting[15];  // hn2t3 2l GPGGA line
-extern int k = 0;               // iterator on the cutting line
 
-void coordinates() {
-    line[0] = '$';
-    line[1] = 'G';
-    line[2] = 'P';
-    line[3] = 'G';
-    line[4] = 'G';
-    line[5] = 'A';
-    while (1) {
-        char c = readUART();
-        i = 5;
-        if (c == '$') {
-            c = readUART();
-            if (c == 'G') {
-                c = readUART();
-                if (c == 'P') {
-                    c = readUART();
+                                
+                         #include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+
+extern char GPSM[150]; //to save GPS message
+extern char *data[15]; //to save the line that have the coordinate 
+extern int dataLength; //global variable that will be used to check gps fix
+int i;           // iterator on the cutting line
+
+void splitLine() {
+
+  dataLength = 0;
+	data[0] = strtok(GPSM, ",");
+  while (data[dataLength] != NULL && dataLength < 14) {
+		dataLength++;
+    data[dataLength] = strtok(NULL, ",");
+  }
+}
+
+void GetLineHaveCoordinates() {
+  GPSM[0] = '$';
+  GPSM[1] = 'G';
+  GPSM[2] = 'P';
+  GPSM[3] = 'G';
+  GPSM[4] = 'G';
+  GPSM[5] = 'A';
+  while (1) {
+		char c = readUART1();
+    i = 6;
+    if (c == '$') {
+        c = readUART1();
+        if (c == 'G') {
+            c = readUART1();
+            if (c == 'P') {
+                c = readUART1();
+                if (c == 'G') {
+                    c = readUART1();
                     if (c == 'G') {
-                        c = readUART();
-                        if (c == 'G') {
-                            c = readUART();
-                            if (c == 'A') {
-                                c = readUART();
-                                while (c != '\n') {
-                                    line[i++] = c;
-                                    c = readUART(); }
-                                break; }}}}}}}
-      
-    
-                                
-       line_cutting[0] = strtok(line, ",");
-    while (line_cutting[k] != NULL && k < 14) {
-        k++;
-        line_cutting[k] = strtok(NULL, ",");
-    }
+                        c = readUART1();
+                        if (c == 'A') {
+													c = readUART1();
+                            while (c != '\n') {
+															GPSM[i] = c;
+                              i++;
+															c = readUART1();
+                            }
+                            break;
+                        }}}}}}}}
 
-float latitude() { return atof(line_cutting[2]); }
-float longitude() { return atof(line_cutting[4]); }
-                                
-                                
+												
+double getLatitude() { return atof(data[2]); }
+
+double getLongitude() { return atof(data[4]); }
+
+int isOk() {
+    
+  if (*data[6] <= '0') {
+      
+    return 0;
+      
+  } else if ('1' <= *data[6] && *data[6] <= '5') {
+      
+    return 1;
+      
+  } else {      
+    return 0;
+  }
+}      
